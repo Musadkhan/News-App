59eeab4992e8e424129c5da585572d75f3458dc3
Musadkhan <53132240+Musadkhan@users.noreply.github.com>
2022-10-05 12:13:53 +0530

    Now User Can Read Full Article Inside the App With WebView.

--- app/src/main/java/com/example/musadkhan/newsapp/DetailsActivity.java
+++ app/src/main/java/com/example/musadkhan/newsapp/DetailsActivity.java
@@ -2,7 +2,10 @@ package com.example.musadkhan.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +17,8 @@ public class DetailsActivity extends AppCompatActivity {
     NewsHeadlines newsHeadlines;
     TextView titleTxt,autherTxt,timeTxt,detailTxt;
     ImageView newsImg;
+    Button readArticle;
+    public static final String EXTRA_MSG = "com.example.musadkhan.newsapp.extra.MSG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,7 @@ public class DetailsActivity extends AppCompatActivity {
         timeTxt = findViewById(R.id.NewsTime);
         detailTxt = findViewById(R.id.NewsDetail);
         newsImg = findViewById(R.id.newsImage);
+        readArticle = findViewById(R.id.readArticle);
 
         newsHeadlines = (NewsHeadlines) getIntent().getSerializableExtra("data");
 
@@ -35,5 +41,16 @@ public class DetailsActivity extends AppCompatActivity {
         if (newsHeadlines.getUrlToImage()!= null) {
             Picasso.get().load(newsHeadlines.getUrlToImage()).into(newsImg);
         }
+
+
+        readArticle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = newsHeadlines.getUrl();
+                Intent intent = new Intent(DetailsActivity.this,WebScreen.class);
+                intent.putExtra(EXTRA_MSG,url);
+                startActivity(intent);
+            }
+        });
     }
 }
